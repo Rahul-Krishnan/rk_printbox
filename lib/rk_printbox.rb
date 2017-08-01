ce8c6090f0ec01759b0d88ecd1c12502bb9e4574
Rahul Krishnan <rk2211@gmail.com>
2017-08-01 20:14:37 +0530

    Printbox gem

--- lib/rk_printbox.rb
+++ lib/rk_printbox.rb
@@ -1,5 +1,22 @@
 require "rk_printbox/version"
 
 module RkPrintbox
-  # Your code goes here...
+  word = ARGV[0].upcase.chars
+  length = word.length
+
+  word.each do |letter|
+    print "#{letter} "
+  end
+  puts
+
+  (1..(length - 2)).each do |n|
+    print word[n]
+    print " " * (2 * length - 3)
+    puts word[length - n - 1]
+  end
+
+  word.reverse.each do |letter|
+    print "#{letter} "
+  end
+  puts
 end
